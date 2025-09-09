3bd582aaa180d8a798919ab353f716c9c78e670b
gprakashgoud <gprakashgoud611@gmail.com>
2025-09-09 20:50:31 +0530

    Operators

---
+++ Arithmaticoperator.java
@@ -0,0 +1,14 @@
+package com.codegnan.javafundamentals;
+
+public class Arithmaticoperator {
+
+	public static void main(String[] args) {
+int a=10,b=20,c=30;
+String d="codegnan";
+System.out.println(a+b+c+d);
+System.out.println(b+c+d+a);
+System.out.println(c+d+a+b);
+System.out.println(d+a+b+c);
+	}
+
+}
