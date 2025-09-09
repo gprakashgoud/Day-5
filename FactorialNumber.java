af90f2d6f10225323f2f06dc4f6924a6945268d9
gprakashgoud <gprakashgoud611@gmail.com>
2025-09-09 20:46:21 +0530

    Operator

---
+++ FactorialNumber.java
@@ -0,0 +1,20 @@
+package com.codegnan.controlstatements;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number to factorial : ");
+		int number = scanner.nextInt();
+		int result=1;
+		for(int i=1;  i<=number; i++) {
+			result=result*i;
+			System.out.println("factorial of: " +number+ "is" +result);
+		}
+		
+		scanner.close();
+	}
+
+}
